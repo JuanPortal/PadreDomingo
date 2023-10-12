cc5dc531d27170bbd614329dcae6ed983e988141
Portal <juanportalg@gmail.com>
2023-10-11 20:06:05 -0700

    games implemented

--- src/Juegos.jsx
+++ src/Juegos.jsx
@@ -4,6 +4,7 @@ export const Juegos = () => {
   return (
     <section className='juegos'>
         <h2>Juegos</h2>
+        <img src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Venn%20Diagram.webp?alt=media&token=0f8ceeec-6e8e-4897-ac06-608e5b48200e&_gl=1*102nvsk*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NzA3OTY3NC43LjEuMTY5NzA3OTY5OS4zNS4wLjA." title='Venn Diagram of the common games' alt="Venn Diagram of the common games" />
     </section>
   )
 }
