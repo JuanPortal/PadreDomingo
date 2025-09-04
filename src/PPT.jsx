a88c2933b0eb88db9d7b4c6c2aead6deee1b842a
Juan Portal <juanportalg@gmail.com>
2025-09-03 22:55:40 -0700

    fixing PPTs URLs and CSS

--- src/PPT.jsx
+++ src/PPT.jsx
@@ -1,97 +1,115 @@
 import React from 'react'
 
 export const PPT = () => {
-    function handleDownload(member, number) {
-        const fileUrl = `./public/Velada ${number}/${member}.pptx`;
-        const fileName = `${member}.pptx`;
-        const anchor = document.createElement('a');
-        anchor.href = fileUrl;
-        anchor.download = fileName;
-        anchor.click();
-        anchor.remove();
-        console.log(`Downloaded:    assets/Velada ${number}/${member}.pptx`)
-    };
-
-
     return (
         <section className='ppt'>
             <h2>PPT</h2>
-            <h3>Velada  1</h3>
-            <ul>
-                <li>
-                    <p>Alexis</p>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%201.png?alt=media&token=f3096008-e227-4b7f-8eb1-9fa31df7f655&_gl=1*c6033q*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkxOTIxNS41LjEuMTY5NjkxOTIyNC41MS4wLjA." title="Alexis 1" alt="Alexis 1" />
-                </li>
-                <li>
-                    <p>Farge</p>
-                    <img onClick={() => handleDownload('Farge', 1)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%201.png?alt=media&token=fef6e9ac-2679-4346-af6e-43d631d155ed&_gl=1*1c4c0jw*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkwOTI2My4xOS4wLjA." title="Farge 1" alt="Farge 1" />
-                </li>
-                <li>
-                    <p>Mapache</p>
-                    <img onClick={() => handleDownload('Mapache', 1)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%201.png?alt=media&token=dd1dadfe-f262-4fd2-a190-9bfeafa98e9b&_gl=1*ybinrh*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDAyMS4yOC4wLjA." title="Mapache 1" alt="Mapache 1" />
-                </li>
-                <li>
-                    <p>Portal</p>
-                    <img onClick={() => handleDownload('Portal', 1)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%201.png?alt=media&token=daa29a76-da44-4d3e-8b41-215ec52f53f9&_gl=1*58u7u3*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDA0MS44LjAuMA.." title="Portal 1" alt="Portal 1" />
-                </li>
-            </ul>
-            <h3>Velada 2</h3>
-            <ul>
-                <li>
-                    <p>Alexis</p>
-                    <img onClick={() => handleDownload('Alexis', 2)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%202.png?alt=media&token=74fe10d4-3ddf-4d92-83e7-60ea800e8fce&_gl=1*pxpepv*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDAwOS40MC4wLjA." title="Alexis 2" alt="Alexis 2" />
-                </li>
-                <li>
-                    <p>Farge</p>
-                    <img onClick={() => handleDownload('Farge', 2)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%202.png?alt=media&token=6b574e37-e75f-456b-8720-cecf36069bd5&_gl=1*tdd6ze*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkwOTk4OS42MC4wLjA." title="Farge 2" alt="Farge 2" />
-                </li>
-                <li>
-                    <p>Mapache</p>
-                    <img onClick={() => handleDownload('Mapache', 2)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%202.png?alt=media&token=406479c3-3098-40f9-ba2d-5fb05c7a7d65&_gl=1*199alnv*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDAzNC4xNS4wLjA." title="Mapache 2" alt="Mapache 2" />
-                </li>
-                <li>
-                    <p>Portal</p>
-                    <img onClick={() => handleDownload('Portal', 2)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%202.png?alt=media&token=4d31d7d0-1181-44fa-a069-f73dddd97545&_gl=1*s195nv*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDA1MS42MC4wLjA." title="Portal 2" alt="Portal 2" />
-                </li>
-            </ul>
-            <h3>Velada 3</h3>
-            <ul>
-                <li>
-                    <p>Alexis</p>
-                    <img onClick={() => handleDownload('Alexis', 3)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%203.png?alt=media&token=ca285c1c-2729-4a5c-97cf-9a017d04d960" title="Alexis 3" alt="Alexis 3" />
-                </li>
-                <li>
-                    <p>Farge</p>
-                    <img onClick={() => handleDownload('Farge', 3)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%203.png?alt=media&token=a765e987-3ff3-4689-b100-e0c49892b2f6" title="Farge 3" alt="Farge 3" />
-                </li>
-                <li>
-                    <p>Mapache</p>
-                    <img onClick={() => handleDownload('Mapache', 3)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%203.png?alt=media&token=cae5e7ba-737c-416e-9aff-655d7374ed4c" title="Mapache 3" alt="Mapache 3" />
-                </li>
-                <li>
-                    <p>Portal</p>
-                    <img onClick={() => handleDownload('Portal', 3)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%203.png?alt=media&token=383ef242-26af-4aa2-b64f-d362935c1d73" title="Portal 3" alt="Portal 3" />
-                </li>
-            </ul>
-            <h3>Velada 4</h3>
-            <ul>
-                <li>
-                    <p>Alexis</p>
-                    <img onClick={() => handleDownload('Alexis', 4)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%204.png?alt=media&token=ab3314c8-6c80-4c3a-9e16-ee0d02e4f1d5" title="Alexis 4" alt="Alexis 4" />
-                </li>
-                <li>
-                    <p>Farge</p>
-                    <img onClick={() => handleDownload('Farge', 4)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%204.png?alt=media&token=22f33e6e-d6a1-4911-9130-4e421bb66dc8" title="Farge 4" alt="Farge 4" />
-                </li>
-                <li>
-                    <p>Mapache</p>
-                    <img onClick={() => handleDownload('Mapache', 4)} src="" title="Mapache 4" alt="Mapache 4" />
-                </li>
-                <li>
-                    <p>Portal</p>
-                    <img onClick={() => handleDownload('Portal', 4)} src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%204.png?alt=media&token=ad1c1924-0026-4e50-9fea-a6f9de83b54c" title="Portal 4" alt="Portal 4" />
-                </li>
-            </ul>
+            <div className="velada">
+                <h3>Velada  1</h3>
+                <h4>26 agosto 2022</h4>
+                <ul>
+                    <li>
+                        <p>Alexis</p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%201.png?alt=media&token=f3096008-e227-4b7f-8eb1-9fa31df7f655&_gl=1*c6033q*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkxOTIxNS41LjEuMTY5NjkxOTIyNC41MS4wLjA." title="Alexis 1" alt="Alexis 1" />
+                    </li>
+                    <li>
+                        <p>Farge</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1iJJm3hnyRll-OL5f7yUe7b06o4jv-BGK/edit?usp=share_link&ouid=108757625670839719398&rtpof=true&sd=true", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%201.png?alt=media&token=fef6e9ac-2679-4346-af6e-43d631d155ed&_gl=1*1c4c0jw*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkwOTI2My4xOS4wLjA." title="Farge 1" alt="Farge 1" />
+                    </li>
+                    <li>
+                        <p>Mapache</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1iOXU_KgMRkX8PAXm-tSLai1SjfpC1glO/edit?usp=share_link&ouid=108757625670839719398&rtpof=true&sd=true", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%201.png?alt=media&token=dd1dadfe-f262-4fd2-a190-9bfeafa98e9b&_gl=1*ybinrh*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDAyMS4yOC4wLjA." title="Mapache 1" alt="Mapache 1" />
+                    </li>
+                    <li>
+                        <p>Portal</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1l6sq8EC63vvGutr3dBFErIjKDOXf9YNERTpx9efuJK8/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%201.png?alt=media&token=daa29a76-da44-4d3e-8b41-215ec52f53f9&_gl=1*58u7u3*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDA0MS44LjAuMA.." title="Portal 1" alt="Portal 1" />
+                    </li>
+                </ul>
+            </div>
+            
+            <div className="velada">
+                <h3>Velada 2</h3>
+                <h4>X X 2023</h4>
+                <ul>
+                    <li>
+                        <p>Alexis</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1KqEK1IeVuBk1HaxDTLUa4vz5EDFS6ohEJUweexBLsZg/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%202.png?alt=media&token=74fe10d4-3ddf-4d92-83e7-60ea800e8fce&_gl=1*pxpepv*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDAwOS40MC4wLjA." title="Alexis 2" alt="Alexis 2" />
+                    </li>
+                    <li>
+                        <p>Farge</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1d4QUCrit6mzE4mg4xL5Ov21aw8CjX46D/edit?usp=share_link&ouid=108757625670839719398&rtpof=true&sd=true", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%202.png?alt=media&token=6b574e37-e75f-456b-8720-cecf36069bd5&_gl=1*tdd6ze*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkwOTk4OS42MC4wLjA." title="Farge 2" alt="Farge 2" />
+                    </li>
+                    <li>
+                        <p>Mapache</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1cRutYYJP2b8o6FS_Mx7ohf8S9wEBDcP9/edit?usp=share_link&ouid=108757625670839719398&rtpof=true&sd=true", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%202.png?alt=media&token=406479c3-3098-40f9-ba2d-5fb05c7a7d65&_gl=1*199alnv*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDAzNC4xNS4wLjA." title="Mapache 2" alt="Mapache 2" />
+                    </li>
+                    <li>
+                        <p>Portal</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1UHIg8siW6dFROyssN9t1WIEcCOLX8KVQ6wnMw5uKKP4/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%202.png?alt=media&token=4d31d7d0-1181-44fa-a069-f73dddd97545&_gl=1*s195nv*_ga*Mzg5Mjk2OTcuMTY5Njg4OTA0Mg..*_ga_CW55HF8NVT*MTY5NjkwNzY2NC4zLjEuMTY5NjkxMDA1MS42MC4wLjA." title="Portal 2" alt="Portal 2" />
+                    </li>
+                </ul>
+            </div>
+            
+            <div className="velada">
+                <h3>Velada 3</h3>
+                <h4>5 noviembre 2023</h4>
+                <ul>
+                    <li>
+                        <p>Alexis</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1TQFYCAG3MgdldJdWxst4vUj711WENGz5/edit?usp=share_link&ouid=108757625670839719398&rtpof=true&sd=true", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%203.png?alt=media&token=ca285c1c-2729-4a5c-97cf-9a017d04d960" title="Alexis 3" alt="Alexis 3" />
+                    </li>
+                    <li>
+                        <p>Farge</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1dJnkyph2VKxbWCpo1JnVibzXCDeU7d86bBR7_jgUMHI/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%203.png?alt=media&token=a765e987-3ff3-4689-b100-e0c49892b2f6" title="Farge 3" alt="Farge 3" />
+                    </li>
+                    <li>
+                        <p>Mapache</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1fVR5Gjqs74hOrtTwLy6niREwrlgJ9Fie/edit?usp=share_link&ouid=108757625670839719398&rtpof=true&sd=true", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%203.png?alt=media&token=cae5e7ba-737c-416e-9aff-655d7374ed4c" title="Mapache 3" alt="Mapache 3" />
+                    </li>
+                    <li>
+                        <p>Portal</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1tZAuz93G5lBcolw8U19EzSmAVHze8i0ex8n7a1fRe0s/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%203.png?alt=media&token=383ef242-26af-4aa2-b64f-d362935c1d73" title="Portal 3" alt="Portal 3" />
+                    </li>
+                </ul>
+            </div>
+
+            <div className="velada">
+                <h3>Velada 4</h3>
+                <h4>18 mayo 2024</h4>
+                <ul>
+                    <li>
+                        <p>Alexis</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1IykUMBERh2UgCcTe4Vw3B1yVHMmFSF03RYln90BYeb8/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Alexis%204.png?alt=media&token=ab3314c8-6c80-4c3a-9e16-ee0d02e4f1d5" title="Alexis 4" alt="Alexis 4" />
+                    </li>
+                    <li>
+                        <p>Farge</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1o2X7pchllEudbHipmZgvyOaXvL5qi7S12kdnggYHDvk/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Farge%204.png?alt=media&token=22f33e6e-d6a1-4911-9130-4e421bb66dc8" title="Farge 4" alt="Farge 4" />
+                    </li>
+                    <li>
+                        <p>Mapache</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/1G6Kwn2yiGZI4Qxgq_QjD9zEFwgSoxV4_22o9HOFQsOI/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Mapache%204.png?alt=media&token=c5a97be5-4119-40ea-9c48-0fe644585ce3" title="Mapache 4" alt="Mapache 4" />
+                    </li>
+                    <li>
+                        <p>Portal</p>
+                        <img onClick={() => window.open("https://docs.google.com/presentation/d/17gVzholBZDcySXUM5ito_v4iizM_P0NI4zaOAg1nGAY/edit?usp=share_link", "_blank")}
+                        src="https://firebasestorage.googleapis.com/v0/b/padre-domingo.appspot.com/o/Portal%204.png?alt=media&token=ad1c1924-0026-4e50-9fea-a6f9de83b54c" title="Portal 4" alt="Portal 4" />
+                    </li>
+                </ul>
+            </div>
         </section>
     )
 }
